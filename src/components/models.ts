255fef282845cbcb3e93be63efbabfef787cc23d
dacmarcell <dacmarcell@gmail.com>
2025-01-07 00:09:38 -0300

    chore: add task types

--- src/components/models.ts
+++ src/components/models.ts
@@ -1,8 +1,21 @@
-export interface Todo {
-  id: number;
-  content: string;
+export type Task = {
+  id: number
+  title: string
+  description: string
+  created_at: Date
+  updated_at: Date
+  finished_at: Date
+  status: TaskStatus
+}
+
+export type CreateTask = Omit<Task, 'id' | 'created_at' | 'updated_at' | 'finished_at'>
+
+enum TaskStatus {
+  Pending = 'pending',
+  InProgress = 'in-progress',
+  Finished = 'finished',
 }
 
 export interface Meta {
-  totalCount: number;
+  totalCount: number
 }

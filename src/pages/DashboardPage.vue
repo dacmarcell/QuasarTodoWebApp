8728087e6278c8cdfb94ec260cd94ccf19937af2
dacmarcell <dacmarcell@gmail.com>
2025-01-07 02:30:32 -0300

    chore: add dashboard page

---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <h1>dashboard</h1>
+  </q-page>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'DashboardPage',
+}
+</script>

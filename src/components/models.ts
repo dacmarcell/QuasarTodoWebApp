af04977042679de4fb831d725bea6f3eaf92cf28
dacmarcell <dacmarcell@gmail.com>
2025-01-08 02:06:59 -0300

    feat: extend Task model to include categories and update CreateTask type for category handling

--- src/components/models.ts
+++ src/components/models.ts
@@ -6,9 +6,25 @@ export type Task = {
   updated_at: Date
   finished_at: Date
   status: TaskStatus
+  categories: Categories[]
+}
+
+type Categories = {
+  id: number
+  name: string
+  task_id: number
+  created_at: Date
+  updated_at: Date
 }
 
 export type TaskStatus = 'pending' | 'in-progress' | 'finished'
 
-export type CreateTask = Omit<Task, 'id' | 'created_at' | 'updated_at' | 'finished_at'>
-export type UpdateTask = Partial<CreateTask>
+export type CreateTask = Omit<
+  Task,
+  'id' | 'created_at' | 'updated_at' | 'finished_at' | 'categories'
+> & {
+  category_name: string
+}
+export type UpdateTask = Partial<
+  Omit<Task, 'id' | 'created_at' | 'updated_at' | 'finished_at' | 'categories'>
+>

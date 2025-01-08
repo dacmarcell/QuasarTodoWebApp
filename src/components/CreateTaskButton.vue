7729ef50a3f5a75b146296e789316431e608500d
dacmarcell <dacmarcell@gmail.com>
2025-01-07 23:08:12 -0300

    chore: add create task button

---
+++ src/components/CreateTaskButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-btn
+    label="Adicionar Tarefa"
+    color="primary"
+    icon="add"
+    class="q-ma-md"
+    @click="handleOpenDialog()"
+  />
+</template>
+
+<script lang="ts">
+export default {
+  name: 'CreateTaskButton',
+  props: {
+    handleOpenDialog: {
+      type: Function,
+      required: true,
+    },
+  },
+}
+</script>

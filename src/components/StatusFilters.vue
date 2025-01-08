e9c235d9d0df7417c4bb4e1204dfb8878c57c51c
dacmarcell <dacmarcell@gmail.com>
2025-01-07 21:02:02 -0300

    chore: change filter from buttons and send to store

--- src/components/StatusFilters.vue
+++ src/components/StatusFilters.vue
@@ -2,40 +2,62 @@
   <div class="row items-center justify-center q-mb-md">
     <q-btn-group flat>
       <q-btn
-        v-for="option in statusOptions"
+        v-for="statusOptions in statusOptions"
         flat
-        :key="option.value"
-        :label="option.label"
-        :outline="filter !== option.value"
-        @click="filter = filter === option.value ? null : option.value"
-        :color="filter === option.value ? statusColors[option.value as TaskStatus] : 'grey-5'"
+        :key="statusOptions.value"
+        :label="statusOptions.label"
+        :outline="filter !== statusOptions.value"
+        @click="handleChangeFilter(statusOptions)"
+        :color="
+          filter === statusOptions.value
+            ? statusColors[statusOptions.value as TaskStatus]
+            : 'grey-5'
+        "
       />
     </q-btn-group>
   </div>
 </template>
 
 <script lang="ts">
-import { ref } from 'vue'
-import { TaskStatus } from './models'
+import { computed } from 'vue'
+import { useTasksStore } from 'src/stores/task-store'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { FilterStatus, TaskStatus } from './models'
+
+type StatusOption = { label: string; value: FilterStatus }
 
 export default {
   name: 'StatusFilters',
   setup() {
-    const filter = ref<TaskStatus | null>(null)
+    const tasksStore = useTasksStore()
 
-    const statusOptions = [
+    const filter = computed({
+      get: () => tasksStore.filter,
+      set: (value) => {
+        tasksStore.setFilter(value)
+      },
+    })
+
+    const statusOptions: StatusOption[] = [
+      { label: 'Todos', value: 'all' },
       { label: 'Pendente', value: 'pending' },
       { label: 'Em progresso', value: 'in-progress' },
       { label: 'Concluída', value: 'finished' },
     ]
 
     const statusColors = {
+      all: 'purple',
       pending: 'orange',
       'in-progress': 'blue',
       finished: 'green',
     }
 
-    return { filter, statusOptions, statusColors }
+    function handleChangeFilter(statusOption: StatusOption) {
+      const filterOption = tasksStore.filter === statusOption.value ? null : statusOption.value
+      tasksStore.setFilter(filterOption)
+    }
+
+    return { filter, statusOptions, statusColors, handleChangeFilter }
   },
 }
 </script>

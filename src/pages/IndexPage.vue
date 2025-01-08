c6c143892c92e114683e13704fdabe42eb53395d
dacmarcell <dacmarcell@gmail.com>
2025-01-08 00:13:02 -0300

    refactor: move main page to index page component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,52 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <TodosList />
+    <q-page padding>
+      <CreateTaskButton
+        :isCreateTaskFormOpen="isCreateTaskFormOpen"
+        @update:isCreateTaskFormOpen="isCreateTaskFormOpen = $event"
+      />
+      <CreateTaskDialog
+        :isCreateTaskFormOpen="isCreateTaskFormOpen"
+        @update:isCreateTaskFormOpen="isCreateTaskFormOpen = $event"
+      />
+      <CleanAllTasksButton />
+      <q-separator spaced />
+      <StatusFilters />
+      <TaskChips />
+      <TaskList />
+    </q-page>
   </q-page>
 </template>
 
 <script lang="ts">
-import TodosList from 'components/TodosList.vue'
+import { ref } from 'vue'
+
+import CleanAllTasksButton from 'components/CleanAllTasksButton.vue'
+import CreateTaskDialog from 'components/CreateTaskDialog.vue'
+import CreateTaskButton from 'components/CreateTaskButton.vue'
+import StatusFilters from 'components/StatusFilters.vue'
+import TaskChips from 'components/TaskChips.vue'
+import TaskList from 'components/TaskList.vue'
 
 export default {
   name: 'IndexPage',
+  setup() {
+    const isCreateTaskFormOpen = ref(false)
+    return { isCreateTaskFormOpen }
+  },
   components: {
-    TodosList,
+    CleanAllTasksButton,
+    CreateTaskDialog,
+    CreateTaskButton,
+    StatusFilters,
+    TaskChips,
+    TaskList,
   },
 }
 </script>
+
+<style scoped>
+.q-page {
+  width: 100%;
+}
+</style>

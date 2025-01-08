28c22fbc09479b96aaeabbae8086a55e31070de5
dacmarcell <dacmarcell@gmail.com>
2025-01-07 23:13:52 -0300

    refactor: replace task list implementation with TaskList component and clean up unused functions

--- src/components/TodosList.vue
+++ src/components/TodosList.vue
@@ -15,60 +15,7 @@
       </q-chip>
     </div>
     <!-- Lista de tarefas -->
-    <q-card>
-      <q-list bordered separator>
-        <q-item v-for="task in tasksToDisplay" :key="task.title" clickable class="q-pa-sm">
-          <q-item-section avatar>
-            <q-icon :name="getIcon(task.status)" :color="getStatusColor(task.status)" size="md" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>{{ task.title }}</q-item-label>
-            <q-item-label caption>{{ task.description }}</q-item-label>
-            <q-item-label caption> Criado {{ formatDate(task.created_at) }} </q-item-label>
-            <q-item-label v-if="task.finished_at" caption>
-              Concluído em: {{ formatDate(task.finished_at) }}
-            </q-item-label>
-          </q-item-section>
-          <q-item-section side>
-            <q-chip :color="getStatusColor(task.status)" outline class="q-ma-sm" size="sm">
-              {{ formatStatusName(task.status) }}
-            </q-chip>
-
-            <div>
-              <q-btn
-                flat
-                size="sm"
-                icon="start"
-                color="black"
-                class="q-ma-xs"
-                @click="updateTask(task.id, { status: 'in-progress' }, $q)"
-                v-if="task.status !== 'in-progress' && task.status !== 'finished'"
-                ><q-tooltip>Iniciar tarefa</q-tooltip></q-btn
-              >
-              <q-btn
-                v-if="task.status !== 'finished' && task.status !== 'pending'"
-                icon="check_circle"
-                color="green"
-                flat
-                size="sm"
-                class="q-ma-xs"
-                @click="updateTask(task.id, { status: 'finished' }, $q)"
-                ><q-tooltip>Completar tarefa</q-tooltip></q-btn
-              >
-              <q-btn
-                icon="delete"
-                color="red"
-                flat
-                size="sm"
-                class="q-ma-xs"
-                @click="deleteTask(task.id, $q)"
-                ><q-tooltip>Deletar tarefa</q-tooltip></q-btn
-              >
-            </div>
-          </q-item-section>
-        </q-item>
-      </q-list>
-    </q-card>
+    <TaskList />
     <q-dialog v-model="isCreateTaskFormOpen">
       <q-card style="min-width: 350px">
         <q-card-section>
@@ -104,14 +51,14 @@
 <script lang="ts">
 import { computed, onMounted, ref, watch } from 'vue'
 import { useQuasar } from 'quasar'
-import { formatDistanceToNow } from 'date-fns'
-import { ptBR } from 'date-fns/locale'
 
-import { CreateTask, Task, TaskStatus } from './models'
-import StatusFilters from 'components/StatusFilters.vue'
+import { CreateTask, Task } from './models'
 import { useTasksStore } from 'src/stores/task-store'
+
 import CleanAllTasksButton from './CleanAllTasksButton.vue'
 import CreateTaskButton from './CreateTaskButton.vue'
+import StatusFilters from './StatusFilters.vue'
+import TaskList from './TaskList.vue'
 
 export default {
   name: 'TodosList',
@@ -168,50 +115,6 @@ export default {
       }
     }
 
-    const formatDate = (date?: Date) => {
-      if (!date) return 'N/A'
-      return formatDistanceToNow(date, { addSuffix: true, locale: ptBR })
-    }
-
-    const formatStatusName = (status: TaskStatus) => {
-      switch (status) {
-        case 'pending':
-          return 'Pendente'
-        case 'in-progress':
-          return 'Em progresso'
-        case 'finished':
-          return 'Concluída'
-        default:
-          return 'Desconhecido'
-      }
-    }
-
-    const getStatusColor = (status: TaskStatus) => {
-      switch (status) {
-        case 'pending':
-          return 'orange'
-        case 'in-progress':
-          return 'blue'
-        case 'finished':
-          return 'green'
-        default:
-          return 'grey'
-      }
-    }
-
-    const getIcon = (status: TaskStatus) => {
-      switch (status) {
-        case 'pending':
-          return 'hourglass_empty'
-        case 'in-progress':
-          return 'loop'
-        case 'finished':
-          return 'check_circle'
-        default:
-          return 'help'
-      }
-    }
-
     watch(
       () => tasksStore.tasks,
       async () => {
@@ -231,28 +134,25 @@ export default {
     })
 
     return {
-      tasksToDisplay,
-      filteredTasks: tasksStore.filteredTasks,
-      deleteTask: tasksStore.deleteTask,
-      updateTask: tasksStore.updateTask,
-      createTask: tasksStore.createTask,
-      getIcon,
       newTask,
-      formatDate,
       allTasksCount,
-      getStatusColor,
+      tasksToDisplay,
+      handleOpenDialog,
       handleCreateTask,
-      formatStatusName,
       handleCloseDialog,
       inProgressTasksCount,
       isCreateTaskFormOpen,
-      handleOpenDialog,
+      deleteTask: tasksStore.deleteTask,
+      updateTask: tasksStore.updateTask,
+      createTask: tasksStore.createTask,
+      filteredTasks: tasksStore.filteredTasks,
     }
   },
   components: {
-    StatusFilters,
     CleanAllTasksButton,
     CreateTaskButton,
+    StatusFilters,
+    TaskList,
   },
 }
 </script>
